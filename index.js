fe316da8ae239d9c527d126405ae72c43d6e8bd8
VescureTwo <83725273+VescureTwo@users.noreply.github.com>
2023-01-01 09:53:41 +1300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(function(req, res, next){
+   var data = "";
+   req.on('data', function(chunk){ data += chunk})
+   req.on('end', function(){
+      req.rawBody = data;
+      next();
+   })
+})
 
+// Your route registration:
+app.get('/', function(){// whatever...})
 
-
-app.post('/post-test', function(req, res){
-   console.log(req.body);
-   res.status(200);
-});
-
-app.listen(8080, () => console.log(`Started server at http://localhost:8080!`));
+app.post('/test', function(req, res){
+    console.log(req.rawBody);
+    res.send("your request raw body is:"+req.rawBody);
+})
