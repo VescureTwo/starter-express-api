1e5a8496c0c0f83143c8bc2d2807f6e9312987bb
VescureTwo <83725273+VescureTwo@users.noreply.github.com>
2023-01-01 08:50:19 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/post-test', (req, res) => {
+    console.log('Got body:', req.body);
+    res.sendStatus(200);
+});
+
+app.listen(8080, () => console.log(`Started server at http://localhost:8080!`));
