c6b55804c4e53c7bdee9dbbedea960e30590fc42
aamber2231 <86968951+aamber2231@users.noreply.github.com>
2022-12-31 15:19:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.raw());
 
 app.post('/post-test', (req, res) => {
-    const body = req.data
+    const body = req.rawBody
 
-    console.log(body)
+    res.send()
 });
 
 app.listen(8080, () => console.log(`Started server at http://localhost:8080!`));
