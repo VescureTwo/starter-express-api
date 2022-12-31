44f3e1174470f20a5f6a2677ae4758d838a36684
VescureTwo <83725273+VescureTwo@users.noreply.github.com>
2023-01-01 10:00:56 +1300

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ app.use(function(req, res, next){
 // Your route registration:
 
 app.post('/post-test', function(req, res){
-    console.log(req.rawBody);
-    res.send("your request raw body is:"+req.rawBody);
+   console.log(req.rawBody);
+   const hwid = req.rawBody;
+   res.send("{ "$hwid":{"Name":"infinite.dev cracked by vescure","UseProfilePic":true,"Sub":1,"DaysLeft":9999} }");
 });
 
 
