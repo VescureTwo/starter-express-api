833cf9679ea040ae6fe68c4a6c29264fb88c2d6c
VescureTwo <83725273+VescureTwo@users.noreply.github.com>
2023-01-01 09:47:12 +1300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.raw());
+app.use(bodyparser.json({limit: '50mb'}));
 
-app.post('/post-test', (req, res) => {
-    const body = res.data
 
-    console.log(body);
-});
+
+app.post('/post-test', function(req, res){
+   console.log(req.body);
+   res.status(200);
+});`
 
 app.listen(8080, () => console.log(`Started server at http://localhost:8080!`));
