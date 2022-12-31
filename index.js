41c86b8fcadfc41fb0c2f7221e3594f9611dc3cd
VescureTwo <83725273+VescureTwo@users.noreply.github.com>
2023-01-01 09:49:36 +1300

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.use(bodyparser.json({limit: '50mb'}));
 app.post('/post-test', function(req, res){
    console.log(req.body);
    res.status(200);
-});`
+});
 
 app.listen(8080, () => console.log(`Started server at http://localhost:8080!`));
